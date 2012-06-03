abd7b32f1024041c9d98a0830192bd4a59e818c1
Corey Nolet <cnolet@texeltek.com>
2012-06-02 21:15:29 -0400

    Added the necessary inputs to set up a map/reduce job with the GemCloudbaseTriplestoreEntityOutputFormat.

--- src/main/java/sonixbp/sync/GemCloudbaseTriplestoreEntityOutputFormat.java
+++ src/main/java/sonixbp/sync/GemCloudbaseTriplestoreEntityOutputFormat.java
@@ -5,6 +5,8 @@ import cloudbase.core.client.CBException;
 import cloudbase.core.client.CBSecurityException;
 import cloudbase.core.client.Connector;
 import cloudbase.core.client.ZooKeeperInstance;
+import cloudbase.core.util.ArgumentChecker;
+import org.apache.commons.codec.binary.Base64;
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.JobContext;
@@ -32,12 +34,35 @@ public class GemCloudbaseTriplestoreEntityOutputFormat extends OutputFormat {
     private static final String INSTANCE_NAME = PREFIX + ".instanceName";
     private static final String ZOOKEEPERS = PREFIX + ".zooKeepers";
 
+    public static void setOutputInfo(JobContext job, String username, byte[] password) {
+
+        Configuration conf = job.getConfiguration();
+        conf.set(USERNAME, username);
+        conf.set(PASSWORD, new String(Base64.encodeBase64(password)));
+        conf.setBoolean(OUTPUT_INFO_HAS_BEEN_SET, true);
+    }
+    public static void setZookeeperInstance(JobContext job, String instance, String zookeepers) {
+
+        Configuration conf = job.getConfiguration();
+        conf.set(INSTANCE_NAME, instance);
+        conf.set(ZOOKEEPERS, zookeepers);
+        conf.setBoolean(INSTANCE_HAS_BEEN_SET, true);
+    }
+
     public ZooKeeperInstance getZookeeperInstance(JobContext job) {
 
         String instance = job.getConfiguration().get(INSTANCE_NAME);
         String zookeepers = job.getConfiguration().get(ZOOKEEPERS);
 
         return new ZooKeeperInstance(instance, zookeepers);
+    }
+
+    public String getUsername(JobContext job) {
+        return job.getConfiguration().get(USERNAME);
+    }
+
+    public byte[] getPassword(JobContext job) {
+        return Base64.decodeBase64(job.getConfiguration().get(PASSWORD).getBytes());
     }
 
     public TriplestoreEntityService getEntityService(JobContext job) {
@@ -60,11 +85,34 @@ public class GemCloudbaseTriplestoreEntityOutputFormat extends OutputFormat {
     @Override
     public RecordWriter<Text, BasicEntity> getRecordWriter(TaskAttemptContext taskAttemptContext) throws IOException, InterruptedException {
 
-        return new GemEntityRecordWriter(taskAttemptContext);
+        try {
+
+            return new GemEntityRecordWriter(taskAttemptContext);
+        }
+
+        catch(Exception e) {
+
+            throw new IOException("Error returning new GemEntityRecordWriter");
+        }
     }
 
     @Override
-    public void checkOutputSpecs(JobContext jobContext) throws IOException, InterruptedException {}
+    public void checkOutputSpecs(JobContext job) throws IOException, InterruptedException {
+        Configuration conf = job.getConfiguration();
+        if (!conf.getBoolean(OUTPUT_INFO_HAS_BEEN_SET, false))
+            throw new IOException("Output info has not been set.");
+        if (!conf.getBoolean(INSTANCE_HAS_BEEN_SET, false))
+            throw new IOException("Instance info has not been set.");
+        try {
+            Connector c = getZookeeperInstance(job).getConnector(getUsername(job), getPassword(job));
+            if (!c.securityOperations().authenticateUser(getUsername(job), getPassword(job)))
+                throw new IOException("Unable to authenticate user");
+        } catch (CBException e) {
+            throw new IOException(e);
+        } catch (CBSecurityException e) {
+            throw new IOException(e);
+        }
+    }
 
     @Override
     public OutputCommitter getOutputCommitter(TaskAttemptContext taskAttemptContext) throws IOException, InterruptedException {
